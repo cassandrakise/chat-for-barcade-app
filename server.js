7f8766f5df25063a2b4386e1d38efd69c891b525
Cassie Kise <cassandrakise@gmail.com>
2022-11-22 20:29:28 -0800

    updated with option 2 with socket.io

--- server.js
+++ server.js
@@ -7,17 +7,28 @@ const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
+const io = socketIO(server);
 
-const { Server } = require('ws');
-const wss = new Server({ server });
-
-wss.on('connection', (ws) => {
+io.on('connection', (socket) => {
     console.log('Client connected');
-    ws.on('close', () => console.log('Client disconnected'));
+    socket.on('disconnect', () => console.log('Client disconnected'));
   });
 
-  setInterval(() => {
-    wss.clients.forEach((client) => {
-      client.send(new Date().toTimeString());
-    });
-  }, 1000);
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+
+
+
+
+// const { Server } = require('ws');
+// const wss = new Server({ server });
+
+// wss.on('connection', (ws) => {
+//     console.log('Client connected');
+//     ws.on('close', () => console.log('Client disconnected'));
+//   });
+
+//   setInterval(() => {
+//     wss.clients.forEach((client) => {
+//       client.send(new Date().toTimeString());
+//     });
+//   }, 1000);
