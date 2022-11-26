b2a0c66faad8f5601371affc3b10b59305013ce4
Cassie Kise <cassandrakise@gmail.com>
2022-11-25 18:30:35 -0800

    updated with specific actions post user decisions

--- server.js
+++ server.js
@@ -22,24 +22,58 @@ const server = express()
 });
 
 io.on('connection', (socket) => {
-    console.log('Client connected');
-    socket.on('disconnect', () => console.log('Client disconnected'));
+    socket.on("join", (nick,room) => {
+		socket.join(room);
+		//const id=stringHash(nick);
+		if(!people.hasOwnProperty(room)){
+			people[room]={};
+		}
+		
+		people[room][socket.id] = {
+			nick : nick,
+			id : socket.id
+		};
+		sockmap[socket.id] = {
+			nick : nick,
+			room : room
+		}
+		if(messageque.hasOwnProperty(room)){
+			for(i=0;i<messageque[room].length;i++){
+				io.to(room).emit('message que', messageque[room][i].nick,messageque[room][i].msg);
+			}
+		}
+		if(room=='')
+			socket.emit("update", "You have connected to the default room.");
+		else	
+		socket.emit("update", `You have connected to room ${room}.`);
+		socket.emit("people-list", people[room]);
+		socket.to(room).broadcast.emit("add-person",nick,socket.id);
+		console.log(nick);
+		socket.to(room).broadcast.emit("update", `${nick} has come online. `);
+	});
+
+	socket.on('chat message', (msg,room) => {
+		io.to(room).emit('chat message', people[room][socket.id].nick,msg);
+		if(!messageque.hasOwnProperty(room)){
+			messageque[room]=[]
+		}
+		messageque[room].push({
+			nick : people[room][socket.id].nick,
+			msg : msg
+		})
+		if(messageque[room].length>50)
+			messageque[room].shift()
+	});
+
+	socket.on('disconnect', () => {
+		if(sockmap[socket.id]){
+			const room=sockmap[socket.id].room;
+			socket.to(room).broadcast.emit("update", `${sockmap[socket.id].nick} has disconnected. `);
+			io.emit("remove-person",socket.id);
+			delete people[room][socket.id];
+			delete sockmap[socket.id];	
+		}	
+	});
   });
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
-
-
-
-// const { Server } = require('ws');
-// const wss = new Server({ server });
-
-// wss.on('connection', (ws) => {
-//     console.log('Client connected');
-//     ws.on('close', () => console.log('Client disconnected'));
-//   });
-
-//   setInterval(() => {
-//     wss.clients.forEach((client) => {
-//       client.send(new Date().toTimeString());
-//     });
-//   }, 1000);
